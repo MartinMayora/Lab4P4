6dce40c4de8da1a8704e70fb06f29a6c8fe82a31
CataSegui <seguicata@gmail.com>
2025-06-03 11:54:39 -0300

    Agregue la  interfaz IAltaUsuario en .h

---
+++ IAltaUsuario.h
@@ -0,0 +1,31 @@
+#ifndef IALTAUSUARIO_H
+#define IALTAUSUARIO_H
+
+#include "DTUsuario.h"
+#include "TipoTecho.h"
+#include <string>
+#include <set>
+
+class IAltaUsuario {
+    //en las interfaces agregamos todas las operaciones que corresponden al caso de uso 
+    public:
+        virtual bool altaCliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento) = 0;
+
+        virtual bool altaPropietario(std::string nickname, std::string contrasena,std::string nombre, std::string email, std::string cuentaBancaria, std::string telefono) = 0;
+
+        virtual bool altaInmobiliaria(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string direccion, std::string url,std::string telefono) = 0;
+
+        virtual void altaCasa(std::string direccion, int numeroPuerta, int superficie, int anioConstruccion, bool esPH, TipoTecho techo) = 0;
+
+        virtual void altaApartamento(std::string direccion, int numeroPuerta, int superficie, int anioConstruccion, int piso, bool tieneAscensor,float gastosComunes) = 0;
+
+        virtual std::set<DTUsuario> listarPropietarios() = 0;
+
+        virtual void representarPropietario(std::string nicknamePropietario) = 0;
+
+        virtual void finalizarAltaUsuario() = 0;
+
+        virtual ~IAltaUsuario() {}
+};
+
+#endif
