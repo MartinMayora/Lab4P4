bd6d23730c2d3cd8cbf29e06a8b44f6bf186c1ae
CataSegui <seguicata@gmail.com>
2025-06-03 15:03:10 -0300

    agregue el cpp de controladorUsuario

---
+++ src/ControladorUsuario.cpp
@@ -0,0 +1,23 @@
+#include "../include/ControladorUsuario.h"
+#include "../include/ManejadorUsuario.h"
+#include "../include/Cliente.h"
+#include "../include/Propietario.h"
+#include "../include/Inmobiliaria.h"
+#include "../include/DTUsuario.h"
+
+ControladorUsuario* ControladorUsuario::instancia = NULL;
+
+ControladorUsuario::ControladorUsuario() {
+    this->propietarioRecordado = NULL;
+    this->inmobiliariaRecordada = NULL;
+}
+
+ControladorUsuario* ControladorUsuario::getInstance() {
+    if (instancia == NULL)
+        instancia = new ControladorUsuario();
+    return instancia;
+}
+
+bool ControladorUsuario::altaCliente(std::string nickname, std::string contrasena,std::string nombre, std::string email,std::string apellido, std::string documento) {
+    return ManejadorUsuario::getInstance()->crearCliente(nickname, contrasena, nombre, email, apellido, documento);
+}
